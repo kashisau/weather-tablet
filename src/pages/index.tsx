23accfb98ea8f44cf6a22090a66c07951b2c1537
Kashi Samaraweera <samaraweera.kashi@abc.net.au>
2023-03-18 00:12:35 +1100

    Favicon treatment.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -15,7 +15,8 @@ export default function Home (): ReactElement {
         <title>Weather Tablet Home</title>
         <meta name="description" content="A homepage for the tablet device sitting in the living room, to display the time and some weather data." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/favicon.png" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
       </Head>
         <div className={styles.summary}>
           <Time />
